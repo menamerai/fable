fc1264ea868afe023c327334f4995c02c591ddaf
menamerai <rphananh@gmail.com>
2025-02-08 15:04:23 -0500

    feat: upload endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, File, UploadFile
+from dotenv import load_dotenv
+
+load_dotenv()
+
+storage_path = os.getenv("STORAGE_FOLDER")
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    if file.content_type != "text/plain":
+        return {"error": "Only .txt files are allowed"}
+    
+    file_location = f"./data/{file.filename}"
+    with open(file_location, "wb") as f:
+        f.write(file.file.read())
+    
+    return {"message": f"File '{file.filename}' uploaded successfully"}
