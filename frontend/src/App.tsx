f316a0f858798eaa488d30f6ac32da62ec9ca991
zsheng <zsheng.dev@gmail.com>
2025-02-08 15:26:38 -0500

    feat: add basic library view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
+import Library from '@/components/Library';
 import { ModeToggle } from '@/components/mode-toggle';
-import Story from '@/components/Story';
 import { ThemeProvider } from '@/components/theme-provider';
 
 function App() {
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <div className='absolute top-4 right-4'>
+      <div className='absolute top-4 left-4'>
         <ModeToggle />
       </div>
-      <div className='w-full flex justify-center pt-16 pb-24'>
-        <Story />
+      <div className='w-full flex justify-center'>
+        <Library />
       </div>
     </ThemeProvider>
   );
