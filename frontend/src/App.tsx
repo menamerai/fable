8aeecf3e5ee68ba2e30a87ea1f4dda8903d70e70
zsheng <zsheng.dev@gmail.com>
2025-02-08 15:17:56 -0500

    feat: add darkmode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
+import { ModeToggle } from '@/components/mode-toggle';
 import Story from '@/components/Story';
+import { ThemeProvider } from '@/components/theme-provider';
 
 function App() {
   return (
-    <>
-      <div className='w-full flex justify-center'>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <div className='absolute top-4 right-4'>
+        <ModeToggle />
+      </div>
+      <div className='w-full flex justify-center pt-16 pb-24'>
         <Story />
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
