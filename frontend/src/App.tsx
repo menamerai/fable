7458b85e56d4c46d10cb8a4360dc54c14e8bcdb8
sheng <49238630+zsh-eng@users.noreply.github.com>
2025-02-08 16:11:06 -0500

    Zsheng dev (#6)
    * chore: add story
    
    * feat: add basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,12 @@
 import Library from '@/components/Library';
-import { ModeToggle } from '@/components/mode-toggle';
-import { ThemeProvider } from '@/components/theme-provider';
 
 function App() {
   return (
-    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <div className='absolute top-4 left-4'>
-        <ModeToggle />
-      </div>
+    <>
       <div className='w-full flex justify-center'>
         <Library />
       </div>
-    </ThemeProvider>
+    </>
   );
 }
 
