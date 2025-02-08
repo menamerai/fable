a4a2f21fe96d8a3fd209723dc37aa2077ef4679e
sheng <49238630+zsh-eng@users.noreply.github.com>
2025-02-08 16:19:30 -0500

    Zsheng dev (#7)
    * chore: add story
    
    * feat: add basic routing
    
    * feat: add basic UI

--- frontend/src/components/Story.tsx
+++ frontend/src/components/Story.tsx
@@ -1,19 +1,34 @@
 import { BOOKS } from '@/lib/data';
 import { markdownToHtml } from '@/lib/markdown';
-import { useParams } from 'react-router';
+import { ArrowLeft } from 'lucide-react';
+import { useNavigate, useParams } from 'react-router';
 
 export default function Story() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const book = BOOKS.find((book) => book.id === id);
 
   return (
-    <div className='w-full flex justify-center py-16'>
-      <div
-        className='prose dark:prose-invert'
-        dangerouslySetInnerHTML={{
-          __html: markdownToHtml(book?.markdown ?? 'Book not found'),
-        }}
-      />
+    <div className='w-full flex justify-center py-16 '>
+      <div className='flex items-start gap-4 -translate-x-12'>
+        <button
+          onClick={() => navigate('/')}
+          className='p-2 hover:bg-muted rounded-full hover:-translate-x-2 transition-all cursor-pointer'
+        >
+          <ArrowLeft className='w-6 h-6' />
+        </button>
+        <div
+          className='prose dark:prose-invert w-[800px] prose-headings:font-serif prose-headings:tracking-wide'
+          dangerouslySetInnerHTML={{
+            __html: markdownToHtml(
+              book?.markdown ??
+                `# ${book?.title}\n\n${
+                  book?.author
+                }\n\n${'Book content not found'}`
+            ),
+          }}
+        />
+      </div>
     </div>
   );
 }
