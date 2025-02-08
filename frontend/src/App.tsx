939b7f6b40ec32fc563bee4b0502ed739cddf662
zsheng <zsheng.dev@gmail.com>
2025-02-08 15:55:17 -0500

    chore: add story

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Library from '@/components/Library';
+import Story from '@/components/Story';
 
 function App() {
   return (
     <>
       <div className='w-full flex justify-center'>
-        <Library />
+        {/* <Library /> */}
+        <Story />
       </div>
     </>
   );
