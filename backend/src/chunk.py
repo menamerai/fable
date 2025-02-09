55ddc218ab52b07b8d8180f77f83044624de3328
menamerai <rphananh@gmail.com>
2025-02-08 23:52:33 -0500

    chore: cleanup

--- backend/src/chunk.py
+++ backend/src/chunk.py
@@ -1,5 +1,3 @@
-import getpass
-import os
 from typing import List
 from dotenv import load_dotenv
 from pathlib import Path
@@ -16,22 +14,10 @@ def chunk_text(file_path: Path) -> List[dict]:
         breakpoint_threshold_amount=2,
     )
 
-    # data_path = Path(f"/data/books")
-    # data_path.mkdir(parents=True, exist_ok=True)
-
     with open(file_path, "r") as f:
         lines = f.read()
 
-    # chunk_data = Path("../chunks")
-    # chunk_data.mkdir(parents=True, exist_ok=True)
-
     chunks = text_splitter.split_text(lines)
-
-    # doc_contents = []
-    # for idx, doc in enumerate(docs):
-    #     doc_contents.append(doc.page_content)
-    # with open(chunk_data / f"sample_chunk_{idx}.txt", "w") as f:
-    #     f.write(doc.page_content)
 
     music_path = Path(f"/data/music")
     music_path.mkdir(parents=True, exist_ok=True)
